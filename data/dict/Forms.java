8a999adb72bec683e28cca34a2116e44434b45c2
Maksim Filatov <fms2002@mail.ru>
2022-03-11 10:31:28 +0300

    Delete data directory

--- data/dict/Forms.java
+++
@@ -1,9 +0,0 @@
-package ru.rt.fsom.wfc.data.dict;
-
-public final class Forms {
-
-    private Forms() {
-    }
-    
-    public static final String CROSS_PATCHING_TASK_FORM = "CROSS_PATCHING_TASK";
-}
--- data/oms/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://oms.rt.ru/")
-package ru.rt.fsom.wfc.data.oms;
